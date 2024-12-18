a57feba7b42dbeb2d2790c13665e55d280bd9e08
ficool2 <34815548+ficool2@users.noreply.github.com>
2024-12-17 21:57:59 -0500

    Add Square shader example
    Rename moving Square shader to Spin

---
+++ shadersrc/example_spin_ps2x.hlsl
@@ -0,0 +1,41 @@
+// Example shader that displays a moving, spinning and textured square on the screen
+// Adapted from https://www.shadertoy.com/view/clXyWB
+
+#include "common.hlsl"
+
+#define TAU 6.2831853
+
+#define Time Constants0.x
+
+float4 main( PS_INPUT i ) : COLOR
+{
+	float2 uv = i.baseTexCoord - 0.5;
+    float aspect = TexBaseSize.x / TexBaseSize.y;
+
+    uv.x += (abs(fmod(Time * 0.5, 4.0) - 2.0) - 1.0) * 0.5625;
+    uv.y += (abs(fmod(Time / aspect, 4.0) - 2.0) - 1.0) * (aspect - 0.3333333);
+
+    float3 dir = normalize(float3(uv, 1.0));
+    float3 norm = float3(sin(Time * TAU * 0.25), 0.0, cos(Time * TAU * 0.25));
+
+    uv.x /= norm.z;
+    uv /= uv.x * norm.x + 2.0;
+    uv *= 6.0;
+    
+    uv.x /= aspect;
+    
+    float4 col;
+    if (uv.x < -1.0 || uv.x > 1.0 || uv.y < -1.0 || uv.y > 1.0) 
+	{
+		col = float4(0.0, 0.0, 0.0, 0.0);
+	}
+    else
+	{
+		float4 pix = tex2D(Tex1, uv * 0.5 + 0.5);
+		float alpha = pix.w;
+		col = pix * sqrt(abs(dot(dir, norm)));
+		col.w = alpha;
+	}
+
+	return col;
+}
--- shadersrc/example_square_ps2x.hlsl
+++ shadersrc/example_square_ps2x.hlsl
@@ -1,41 +1,35 @@
-// Example shader that displays a moving, textured square on the screen
+// Example shader that displays a colored square in the middle of the screen
 // Adapted from https://www.shadertoy.com/view/clXyWB
 
 #include "common.hlsl"
 
-#define TAU 6.2831853
-
-#define Time Constants0.x
+bool ContainsSquare(float2 screen_point, float2 square_center, float square_size)
+{
+	float half_size = square_size * 0.5;
+    return screen_point.x >= (square_center.x - half_size) 
+		&& screen_point.x <= (square_center.x + half_size) 
+		&& screen_point.y >= (square_center.y - half_size)
+		&& screen_point.y <= (square_center.y + half_size);
+}
 
 float4 main( PS_INPUT i ) : COLOR
 {
-	float2 uv = i.baseTexCoord - 0.5;
-    float aspect = TexBaseSize.x / TexBaseSize.y;
-
-    uv.x += (abs(fmod(Time * 0.5, 4.0) - 2.0) - 1.0) * 0.5625;
-    uv.y += (abs(fmod(Time / aspect, 4.0) - 2.0) - 1.0) * (aspect - 0.3333333);
-
-    float3 dir = normalize(float3(uv, 1.0));
-    float3 norm = float3(sin(Time * TAU * 0.25), 0.0, cos(Time * TAU * 0.25));
-
-    uv.x /= norm.z;
-    uv /= uv.x * norm.x + 2.0;
-    uv *= 6.0;
-    
-    uv.x /= aspect;
-    
-    float4 col;
-    if (uv.x < -1.0 || uv.x > 1.0 || uv.y < -1.0 || uv.y > 1.0) 
-	{
-		col = float4(0.0, 0.0, 0.0, 0.0);
-	}
-    else
-	{
-		float4 pix = tex2D(Tex1, uv * 0.5 + 0.5);
-		float alpha = pix.w;
-		col = pix * sqrt(abs(dot(dir, norm)));
-		col.w = alpha;
-	}
-
-	return col;
+	// background color with full transparency
+	float4 color = float4(0.0, 0.0, 0.0, 0.0);
+	 
+	// map from normalized to screen cordinates [0, 1] -> [0, size]
+	float2 uv = i.baseTexCoord;
+	float2 screen_point = uv / TexBaseSize;
+	
+	// center of screen in screen coordinates
+	float square_size = 200.0;
+	float2 square_center = (1.0 / TexBaseSize) * 0.5;
+	
+	// check if pixel is inside the square
+    if (ContainsSquare(screen_point, square_center, square_size))
+    {
+        color = float4(1.0, 0.0, 0.0, 1.0);
+    }
+	
+	return color;
 }
