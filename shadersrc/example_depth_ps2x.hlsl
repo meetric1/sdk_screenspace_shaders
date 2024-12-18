e3bb9060505aeb4be8c8515585f82e7c71dae214
ficool2 <34815548+ficool2@users.noreply.github.com>
2024-12-17 22:24:24 -0500

    Add Depth example shader
    Fix Square shader using wrong pixel shader

---
+++ shadersrc/example_depth_ps2x.hlsl
@@ -0,0 +1,11 @@
+// Example shader that displays the depth buffer
+// NOTE: the depth buffer in Source only extends 192 units!
+
+#include "common.hlsl"
+
+float4 main( PS_INPUT i ) : COLOR
+{
+    float4 color = tex2D(TexBase, i.baseTexCoord);
+	float depth = 1.0 - color.w;
+	return float4(depth, depth, depth, 1.0);
+}
