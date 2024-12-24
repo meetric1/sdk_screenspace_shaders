be67d43096669dec09da2bf12cc6cad553545717
ficool2 <34815548+ficool2@users.noreply.github.com>
2024-12-24 12:20:02 -0500

    Better wording

--- shadersrc/common.hlsl
+++ shadersrc/common.hlsl
@@ -23,7 +23,7 @@ const float4 Constants3 : register( c3 );
 const float4 Constants4 : register( c4 );
 
 // eye position in world coordinates
-// WARNING: not available on screen overlays
+// WARNING: appears to return garbage on screen overlays for some users
 // use PlayerPosition VMT proxy as a workaround
 const float4 EyePosition : register( c10 );
 
